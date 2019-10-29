d2861028e5e92388188e63abe1a195ce4995b1ae
Clive Uy <clive.uy@outlook.com>
2019-10-28 19:33:22 -0700

    made console output t

--- cgv3.js
+++ cgv3.js
@@ -36,6 +36,7 @@ function initializeCanvas(){
   var str = document.cookie;
   var n = str.indexOf("timeoutstate=");
   var t = str.slice(14+n,15+n);
+  console.log(t);
   //give it dimensions and context
   gameArea.width = 400;
   gameArea.height = 400;
