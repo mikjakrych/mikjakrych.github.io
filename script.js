9336fc8675abee8c8253870ec313630c09c1ad6a
Clive Uy <clive.uy@outlook.com>
2019-11-01 10:21:26 -0700

    Animated pretty much every content element; added animated underline for links

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ function typer(id, wait){
         setTimeout(innerTyper, randInt(40,70));
       };
     },
-
-    wait,i, txt, length, id);
+    wait,i, txt, length, id
+  );
 }
 
 //function to hide things when they are clicked
@@ -32,7 +32,7 @@ function hideIt(id){
   }
 }
 
-//function to open and close nave when screen is below certain width
+  //function to open and close nave when screen is below certain width
 function toggleNav(navbar_id, navbar_open_class, nav_item_default_class, nav_item_open_class, nav_toggle_id, nav_toggle_open_class, escaper_id, escaper_open_class){
   var navbar = document.getElementById(navbar_id);
   var navitems = document.getElementsByClassName(nav_item_default_class);
@@ -51,9 +51,10 @@ function toggleNav(navbar_id, navbar_open_class, nav_item_default_class, nav_ite
         navitems[i].classList.toggle(nav_item_open_class);
         i ++;
         setTimeout(innerToggleNav,30);
-    }
-  },
-  400)
+      }
+    },
+    400
+  )
 }
 
 //make the little nav opener
@@ -79,4 +80,82 @@ function cross(width){
 function resizeCanvas(){
   var canvas = document.getElementById("mycanvas");
   var canvas_parent = canvas.parentElement;
+}
+
+var animate = 1;
+// animate = confirm("Want to view this webpage as an animated site?");
+
+var titles = document.getElementsByClassName("title");
+var subtitles = document.getElementsByClassName("subtitle");
+var text = document.getElementsByClassName("text");
+var headings = document.getElementsByClassName("heading");
+var buttons = document.getElementsByClassName("btn");
+var dividers = document.getElementsByClassName("divider");
+var lists = document.getElementsByTagName("ul");
+
+var things = new Array();
+
+function pushIntoThings(nodeList){
+  for (var i = 0; i < nodeList.length; i ++){
+    things.push(nodeList[i]);
+  }
+}
+
+pushIntoThings(titles);
+pushIntoThings(subtitles);
+pushIntoThings(text);
+pushIntoThings(headings);
+pushIntoThings(buttons);
+pushIntoThings(dividers);
+pushIntoThings(lists);
+
+function move1(object){
+  object.style.opacity = "0";
+  object.style.transform = "translateX(10%)";
+}
+function move2(object){
+  object.style.opacity = "0";
+  object.style.transform = "scale(.9,.9)";
+}
+function move3(object){
+  object.style.opacity = "0";
+}
+function move4(object){
+  object.style.opacity = "0";
+  object.style.transform = "translateY(10%)";
+}
+function move5(object){
+  object.style.opacity = "0";
+  object.style.transform = "translateX(-10%)";
+}
+function moveAll(){
+  if(animate){
+    var t = window.pageYOffset + window.innerHeight;
+    for(var i = 0; i < things.length; i ++){
+      if(t >= things[i].offsetTop){
+        things[i].style.opacity = "1";
+        things[i].style.transition = "1.5s ease";
+        things[i].style.transform = "initial";
+      }
+      else{
+        var z = i % 5;
+        switch (z) {
+          case 0:
+            move1(things[i]);
+            break;
+          case 1:
+            move2(things[i]);
+            break;
+          case 2:
+            move3(things[i]);
+            break;
+          case 3:
+            move4(things[i]);
+            break;
+          case 4:
+            move5(things[i]);
+        }
+      }
+    }
+  }
 }
