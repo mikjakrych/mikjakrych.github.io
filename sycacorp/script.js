dbaacee069f4427faa64c260d65426f3eff68df9
Clive Uy <clive.uy@outlook.com>
2019-11-16 21:41:04 -0800

    removed excess code in script.js; added media queries to style elements at certain device widths

--- sycacorp/script.js
+++ sycacorp/script.js
@@ -15,32 +15,23 @@ function cross(width){
   0 + "," + a + " " +
   a + "," + a;
 }
-
-//function to open and close nave when screen is below certain width
-function toggleNav(navbar_default_class, navbar_open_class, nav_group_default_class, nav_group_open_class, nav_toggle_class, nav_toggle_open_class){
+function toggleNav(navbar_default_class, navbar_open_class, nav_group_default_class, nav_group_open_class, nav_toggle_class, nav_toggle_open_class, escaper_id, escaper_open_class){
   var navgroup = document.getElementsByClassName(nav_group_default_class)[0];
   var navtoggle = document.getElementsByClassName(nav_toggle_class)[0];
   var navbar = document.getElementsByClassName(navbar_default_class)[0];
-  // var i = 0;
-
+  var escaper =document.getElementById(escaper_id);
   navtoggle.classList.toggle(nav_toggle_open_class);
   navgroup.classList.toggle(nav_group_open_class);
   navbar.classList.toggle(navbar_open_class);
+  escaper.classList.toggle(escaper_open_class);
 }
 var current = 0;
 var timer = setTimeout(nextSlide,5000);
-
 function changeSlide(next_index){
-  // var old_button = document.getElementsByClassName("buttonactive")[0];
-  // var new_button = document.getElementsByClassName("carouselbutton")[next_index];
   var old_slide = document.getElementsByClassName("carouselslideactive")[0];
   var new_slide = document.getElementsByClassName("carouselslide")[next_index];
-
   old_slide.classList.toggle("carouselslideactive");
   new_slide.classList.toggle("carouselslideactive");
-  // old_button.classList.toggle("buttonactive");
-  // new_button.classList.toggle("buttonactive");
-
   current = next_index;
   clearTimeout(timer);
   timer = setTimeout(nextSlide,5000);
@@ -52,26 +43,6 @@ function nextSlide(){
     changeSlide(current + 1); //find the next slide
   }
 }
-
-var up = document.getElementsByClassName("floatup");
-var left = document.getElementsByClassName("floatleft");
-var right = document.getElementsByClassName("floatright");
-var zoom = document.getElementsByClassName("zoom");
-var thingsToMove = new Array;
-var originalTransform = new Array;
-var direction = new Array;
-function pushIntoThingsToMove(nodeList, type){
-  for (var i = 0; i < nodeList.length; i ++){
-    thingsToMove.push(nodeList[i]);
-    originalTransform.push(nodeList[i].style.transform);
-    direction.push(type);
-  }
-}
-pushIntoThingsToMove(up, "up");
-pushIntoThingsToMove(left, "left");
-pushIntoThingsToMove(right, "right");
-pushIntoThingsToMove(zoom, "zoom");
-
 var parallax = document.getElementsByClassName("parallax");
 function mkParallax(){
   for (var i = 0; i < parallax.length; i ++){
@@ -80,48 +51,7 @@ function mkParallax(){
     o.style.backgroundPosition = "50% " + v + "px";
   }
 }
-
-function moveAll(){
-  var t = window.pageYOffset + window.innerHeight;
-  for(var i = 0; i < thingsToMove.length; i ++){
-    var object = thingsToMove[i];
-    if(t < object.offsetTop){
-      var c = direction[i];
-      switch (c) {
-        case "up":
-          object.style.transform = "translateY(50%)";
-          object.style.msTransform = "translateY(50%)";
-          object.style.WebkitTransform = "translateY(50%)";
-          break;
-        case "left":
-          object.style.transform = "translateX(50%)";
-          object.style.msTransform = "translateX(50%)";
-          object.style.WebkitTransform = "translateX(50%)";
-          break;
-        case "right":
-          object.style.transform = "translateX(-50%)";
-          object.style.msTransform = "translateX(-50%)";
-          object.style.WebkitTransform = "translateX(-50%)";
-          break;
-        case "zoom":
-          object.style.transform = "scale(.75,.75)";
-          object.style.msTransform = "scale(.75,.75)";
-          object.style.WebkitTransform = "scale(.75,.75)";
-      }
-    } else{
-      object.style.transform = originalTransform[i];
-      object.style.transition = "2s ease";
-      object.style.WebkitTransition = "2s ease";
-      setTimeout(
-        function(){
-          object.style.transform = originalTransform[i];
-        },
-        2000
-      )
-    }
-  }
-}
 function hidePreloader(){
-  var preloader = document.getElementById("preloader");
-  preloader.style.animationPlayState = "running";
+  var preloader = document.getElementsByClassName("preloader")[0];
+  preloader.classList += " preloaderhidden";
 }
