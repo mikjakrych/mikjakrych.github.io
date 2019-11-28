29a923fad864c112c675c5389fca645713b16187
Clive Uy <clive.uy@outlook.com>
2019-11-27 21:02:14 -0800

    began improving mk home site using advanced tech from sycacorp :)

--- script.js
+++ script.js
@@ -2,22 +2,20 @@
 function randInt(min, max){
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
-
 //function that types out a message in a div based on the div's id and wait time
 function typer(id, wait){
   var i = 0;
   var object = document.getElementById(id);
   var txt = object.innerHTML;
   var length = txt.length;
-  object.style.display = "none";
+  object.style.visibility = "hidden";
   object.innerHTML = "";
   var c = document.cookie;
   if (c.includes(id) == false){
     setTimeout(
       function innerTyper(){
-        object.style.display = "block";
+        object.style.visibility = "visible";
         if (i < length){
-          //object.style.background = "hsl(" + (50+20*Math.sin(i/10)) + ", 100%, 40%)";
           object.innerHTML += txt.charAt(i);
           i++;
           setTimeout(innerTyper, randInt(40,70));
@@ -28,73 +26,59 @@ function typer(id, wait){
   }
 }
 //function that popups something up based one id (used for alert)
-function popup(id){
+function popup(id,wait){
   var object = document.getElementById(id);
-  var op = object.style.opacity;
-  var d = object.style.display;
+  object.style.visibility = "hidden";
   var c = document.cookie;
-  object.style.display = "none";
-  object.style.opacity = "0";
-  object.style.transition = ".6s ease";
-  object.style.WebkitTransition = ".6s ease";
   if (c.includes(id) == false){
     setTimeout(
       function displayIt(){
-        object.style.display = d;
-        setTimeout(
-          function fadeIt(){
-            object.style.opacity = op;
-          },
-          600
-        )
+        var k = "fadeIn .8s ease 0s 1 normal both";
+        object.style.WebkitAnimation = k;
+        object.style.MozAnimation = k;
+        object.style.OAnimation = k;
+        object.style.animation = k;
       },
-      1000
+      wait
     );
   }
 }
 //function to hide things when they are clicked; second argument is used if one does not want the side not to appear in some time
 function hideIt(id, days){
   var object = document.getElementById(id);
-  object.style.transition = ".6s ease";
-  object.style.WebkitTransition = ".6s ease";
-  object.style.opacity = "0";
-  setTimeout(
-    function (){
-      object.style.display = "none";
-    },
-    600
-  )
-  var t = new Date();
-  t.setTime(t.getTime() + days * 24 * 60 * 60 * 1000);
-  var wholecookie = id + "=hidden;expires=" + t.toUTCString() + ";path=/";
-  document.cookie = wholecookie;
+  var k = "fadeOut .8s ease 0s 1 normal both";
+  object.style.WebkitAnimation = k;
+  object.style.MozAnimation = k;
+  object.style.OAnimation = k;
+  object.style.animation = k;
+  if(days){
+    var t = new Date();
+    t.setTime(t.getTime() + days * 24 * 60 * 60 * 1000);
+    var wholecookie = id + "=hidden;expires=" + t.toUTCString() + ";path=/";
+    document.cookie = wholecookie;
+  }
 }
-
-  //function to open and close nave when screen is below certain width
+//function to open and close nave when screen is below certain width
 function toggleNav(navbar_id, navbar_open_class, nav_item_default_class, nav_item_open_class, nav_toggle_id, nav_toggle_open_class, escaper_id, escaper_open_class){
   var navbar = document.getElementById(navbar_id);
   var navitems = document.getElementsByClassName(nav_item_default_class);
   var navtoggle = document.getElementById(nav_toggle_id);
   var escaper =document.getElementById(escaper_id);
   var i = 0;
-
   navbar.classList.toggle(navbar_open_class);
   navtoggle.classList.toggle(nav_toggle_open_class);
   escaper.classList.toggle(escaper_open_class);
-
   setTimeout(
     function innerToggleNav(){
       var navitems = document.getElementsByClassName(nav_item_default_class);
       if (i < navitems.length){
         navitems[i].classList.toggle(nav_item_open_class);
         i ++;
-        setTimeout(innerToggleNav,30);
+        setTimeout(innerToggleNav,80);
       }
     },
-    400
-  )
+    400)
 }
-
 //make the little nav opener
 function cross(width){
   var a = width*2/5;
@@ -113,7 +97,6 @@ function cross(width){
   0 + "," + a + " " +
   a + "," + a;
 }
-
 //function to resize game window
 function resizeCanvas(){
   var canvas = document.getElementById("mycanvas");
@@ -123,92 +106,56 @@ function resizeCanvas(){
     canvas.style.transform = "scale(" + factor + "," + factor + ")"
   }
 }
-
-var animate = 1;
-// animate = confirm("Want to view this webpage as an animated site?");
-
-var titles = document.getElementsByClassName("title");
-var subtitles = document.getElementsByClassName("subtitle");
-var text = document.getElementsByClassName("text");
-var headings = document.getElementsByClassName("heading");
-var buttons = document.getElementsByClassName("btn");
-var dividers = document.getElementsByClassName("divider");
-var lists = document.getElementsByTagName("ul");
-
-var things = new Array();
-
-function pushIntoThings(nodeList){
-  for (var i = 0; i < nodeList.length; i ++){
-    things.push(nodeList[i]);
-  }
-}
-
-pushIntoThings(titles);
-pushIntoThings(subtitles);
-pushIntoThings(text);
-pushIntoThings(headings);
-pushIntoThings(buttons);
-pushIntoThings(dividers);
-pushIntoThings(lists);
-
-function move1(object){
-  object.style.opacity = "0";
-  object.style.transform = "translateX(12px)";
-  object.style.msTransform = "translateX(12px)";
-  object.style.WebkitTransform = "translateX(12px)";
-}
-function move2(object){
-  object.style.opacity = "0";
-  object.style.transform = "scale(.9,.9)";
-  object.style.msTransform = "scale(.9,.9)";
-  object.style.WebkitTransform = "scale(.9,.9)";
-}
-function move3(object){
-  object.style.opacity = "0";
-}
-function move4(object){
-  object.style.opacity = "0";
-  object.style.transform = "translateY(12px)";
-  object.style.msTransform = "translateY(12px)";
-  object.style.WebkitTransform = "translateY(12px)";
-}
-function move5(object){
-  object.style.opacity = "0";
-  object.style.transform = "translateX(-12px)";
-  object.style.msTransform = "translateX(-12px)";
-  object.style.WebkitTransform = "translateX(-12px)";
-}
-function moveAll(){
-  if(animate){
-    var t = window.pageYOffset + window.innerHeight;
-    for(var i = 0; i < things.length; i ++){
-      if(t >= things[i].offsetTop){
-        things[i].style.opacity = "1";
-        things[i].style.transition = "1.5s ease";
-        things[i].style.WebkitTransition = "1.5s ease";
-        things[i].style.transform = "initial";
-        things[i].style.msTransform = "initial";
-        things[i].style.WebkitTransform = "initial";
-      }
-      else{
-        var z = i % 5;
-        switch (z) {
-          case 0:
-            move1(things[i]);
-            break;
-          case 1:
-            move2(things[i]);
-            break;
-          case 2:
-            move3(things[i]);
-            break;
-          case 3:
-            move4(things[i]);
-            break;
-          case 4:
-            move5(things[i]);
-        }
-      }
+var parallax = document.querySelectorAll(".parallax");
+function calcParallax(){
+  for (var i = 0; i < parallax.length; i ++){
+    var o = parallax[i];
+    if(window.innerWidth >= 600){
+      o.style.backgroundPosition = "50% calc(100% + " + (o.offsetTop - window.pageYOffset) / 3 + "px)";
+    } else{
+      o.style.backgroundPosition = "center";
     }
   }
 }
+var animated = document.querySelectorAll("[data-animate]");
+function animate(){
+  var zone = window.pageYOffset + window.innerHeight;
+  for(var i = 0; i < animated.length; i ++){
+    var obj = animated[i];
+    if(zone >= obj.offsetTop){
+      var a = obj.getAttribute("data-animate");
+      var b = obj.getAttribute("data-animate-delay");
+      var c = obj.getAttribute("data-animate-duration")
+      if(!b){b = 0}
+      if(!c){c = 1.2}
+      var k = a + " " + c + "s ease " + b + "s 1 normal both";
+      obj.style.WebkitAnimation = k;
+      obj.style.MozAnimation = k;
+      obj.style.OAnimation = k;
+      obj.style.animation = k;
+      obj.style.visibility = "visible";
+    } else{
+      obj.style.WebkitAnimation = "";
+      obj.style.MozAnimation = "";
+      obj.style.OAnimation = "";
+      obj.style.animation = "";
+      obj.style.visibility = "hidden";
+    }
+  }
+}
+window.onload = function(){
+  animate();
+  initializeCanvas();
+  resizeCanvas();
+  document.getElementById("navtoggle").innerHTML = "<svg><polygon points = '" + cross(35) + "'></polygon></svg>";
+  typer('sidenote',10000);
+}
+window.onscroll = function() {
+  calcParallax();
+  animate();
+  popup('alert',20);
+};
+window.onresize = function() {
+  calcParallax();
+  resizeCanvas();
+};
