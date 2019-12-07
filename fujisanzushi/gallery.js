154fbdcdd4a2086b5376a37747cc3c1f6737d049
Clive Uy <clive.uy@outlook.com>
2019-12-07 13:55:27 -0800

    modified fujisan gallery code so that last row pictures are not super huge

--- fujisanzushi/gallery.js
+++ fujisanzushi/gallery.js
@@ -1,21 +1,26 @@
-var g = document.getElementsByClassName("gallery")[0];
-var gdiv = document.getElementsByClassName("gdiv");
-var gdivlen = gdiv.length;
+var g;
+var gdiv;
+var gdivlen;
 var picarray = new Array;
-for (var i = 0; i < gdivlen; i ++){
-  var div = gdiv[i];
-  var pic = gdiv[i].getElementsByClassName("pic")[0];
-  var calcHeight = 300;
-  var calcWidth = pic.naturalWidth * calcHeight / pic.naturalHeight;
-  console.log(calcHeight + " | " + calcWidth);
-  picarray.push(
-    {
-      div:div,
-      calcWidth:calcWidth,
-      calcHeight:calcHeight
-    }
-  );
+function initializeGallery(){
+  g = document.getElementsByClassName("gallery")[0];
+  gdiv = document.getElementsByClassName("gdiv");
+  gdivlen = gdiv.length;
+  for (var i = 0; i < gdivlen; i ++){
+    var div = gdiv[i];
+    var pic = gdiv[i].getElementsByClassName("pic")[0];
+    var calcHeight = 300;
+    var calcWidth = pic.naturalWidth * calcHeight / pic.naturalHeight;
+    picarray.push(
+      {
+        div:div,
+        calcWidth:calcWidth,
+        calcHeight:calcHeight
+      }
+    );
+  }
 }
+
 function gallery(){
   var i = 0;
   while (i < picarray.length){
@@ -24,19 +29,30 @@ function gallery(){
 }
 function galleryInner(g_width, begin_index){
   var temp_width = 0;
-  var next_index = begin_index;
-  while (temp_width + picarray[next_index].calcWidth <= g_width && next_index < gdivlen - 1){
-    temp_width += picarray[next_index].calcWidth;
-    next_index += 1;
+  var temp_index = begin_index;
+  while (picarray[temp_index]){
+    if(temp_width + picarray[temp_index].calcWidth <= g_width && temp_index < gdivlen){
+      temp_width += picarray[temp_index].calcWidth;
+      temp_index ++;
+    } else{
+      break;
+    }
   }
-  var total_width = 0;
-  for (var i = begin_index; i <= next_index; i ++){
-    total_width += picarray[i].calcWidth;
+  if(temp_index == gdivlen){
+    for (var i = begin_index; i < temp_index; i ++){
+      picarray[i].div.style.width = picarray[i].calcWidth + "px";
+      picarray[i].div.style.height = picarray[i].calcHeight + "px";
+    }
+  } else{
+    var total_width = 0;
+    for (var i = begin_index; i <= temp_index; i ++){
+      total_width += picarray[i].calcWidth;
+    }
+    var shrink_ratio = g_width / total_width;
+    for (var i = begin_index; i <= temp_index; i ++){
+      picarray[i].div.style.width = (picarray[i].calcWidth * shrink_ratio) + "px";
+      picarray[i].div.style.height = (picarray[i].calcHeight * shrink_ratio) + "px";
+    }
   }
-  var shrink_ratio = g_width / total_width;
-  for (var d = begin_index; d <= next_index; d ++){
-    picarray[d].div.style.width = (picarray[d].calcWidth * shrink_ratio) + "px";
-    picarray[d].div.style.height = (picarray[d].calcHeight * shrink_ratio) + "px";
-  }
-  return next_index + 1;
+  return temp_index + 1;
 }
